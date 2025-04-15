718854c7c1627ada17c821d241800fb71f314bee
Musarrat1 <junuarabegum8@gmail.com>
2025-04-15 12:51:34 +0600

    added array

--- array.js
+++ array.js
@@ -1,11 +1,27 @@
-const person2 ={
-    name: "musarrat",
-    age:24
-}
-const{name,age}=person2;
-const team =["fabi","sadia","ruhena"];
-const[n,kinh]=team;
-const newFriends=["hero alom",...team];
-console.log(newFriends);
-console.log(`amer jfdkfdkf ${newFriends[2]} dsfskgfkg BVHBGFHFGH`);
-console.log(`amer jfdkfdkf ${newFriends[1]} dsfskgfkg`);
+// const person2 ={
+//     name: "musarrat",
+//     age:24
+// }
+// const{name,age}=person2;
+// const team =["fabi","sadia","ruhena"];
+// const[n,kinh]=team;
+// const newFriends=["hero alom",...team];
+// console.log(newFriends);
+// console.log(`amer jfdkfdkf ${newFriends[2]} dsfskgfkg BVHBGFHFGH`);
+// console.log(`amer jfdkfdkf ${newFriends[1]} dsfskgfkg`);
+
+// const number =[1,2,3,4,7,9];
+//  const array=number.map(element=>element+1);
+//  console.log(array);
+//  const member = ["rohim","shokin","mita"];
+//  const newArray = member.map((element,index)=>{
+//     console.log(index);
+//     console.log(element);
+//  })
+// const squareArray = number.map(element =>
+// {
+//     return element*element;
+// }
+// );
+// console.log(squareArray);
+
---
+++ person.js
@@ -0,0 +1,15 @@
+const details = {
+    name: "musarrat",
+    age:24,
+    trr: ["fabi","korim","added"],
+    status:"not found",
+    country:"Bnagladesh",
+    biodata:{
+        job: "yes",
+        isMarried:true
+    }
+    
+   
+
+}
+console.log(details["biodata"]["job"]);
